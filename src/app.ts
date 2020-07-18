4c83b8e7318155e2eea2bd17ceed3731c02c54d6
ahmed <ahmed.elghoul@infor.com>
2020-07-18 12:11:51 +0100

    reusable validation

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,44 @@ const autobind = (_target: any, _name: string, descriptor: PropertyDescriptor) =
     return adjDescriptor
 }
 
+//validate
+
+interface Validatable {
+    value: string | number,
+    required?: boolean,
+    minLength?: number,
+    maxLength?: number,
+    max?: number,
+    min?: number
+}
+
+const validate = (ele: Validatable): boolean => {
+    let isValid = true;
+
+    if (ele.required) {
+        isValid = isValid && ele.value.toString().trim().length !== 0
+    }  
+    
+    if (ele.minLength != null && typeof ele.value === 'string') {
+        isValid = isValid && ele.value.trim().length > ele.minLength
+    }
+   
+    if (ele.maxLength != null && typeof ele.value === 'string') {
+        isValid = isValid && ele.value.trim().length < ele.maxLength
+    }
+   
+    if (ele.min != null && typeof ele.value === 'number') {
+        isValid = isValid && ele.value > ele.min
+    }
+  
+    if (ele.max != null && typeof ele.value === 'number') {
+        isValid = isValid && ele.value < ele.max
+    }
+
+
+    return isValid;
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -39,14 +77,52 @@ class ProjectInput {
     }
 
 
-    private submitHandler(event: Event) {
-        event.preventDefault();
-        console.log(this.titleInputElement.value);
+    private getUserInput(): [string, string, number] | undefined {
+        const enteredTitle = this.titleInputElement.value
+        const enteredDescription = this.descriptionInputElement.value
+        const enteredPeople = this.peopleInputElement.value
+
+        if (
+
+            !validate({ value: enteredTitle, required: true, minLength: 5 }) ||
+            !validate({ value: enteredDescription, required: true, minLength: 5 }) ||
+            !validate({ value: +enteredPeople, required: true, min: 5 })
+
+            // enteredTitle.trim().length === 0 ||
+            // enteredDescription.trim().length === 0 ||
+            // enteredPeople.trim().length === 0
+        ) {
+            alert('invalid input, please try agian')
+            return;
+        } else {
+            return [enteredTitle,
+                enteredDescription,
+                +enteredPeople]
+        }
+
+
+    }
+
+
+    private clearInputs() {
+        this.titleInputElement.value = ''
+        this.descriptionInputElement.value = ''
+        this.peopleInputElement.value = ''
     }
 
     @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.getUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
+    }
+
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this))
+        this.element.addEventListener('submit', this.submitHandler)
 
     }
 
@@ -55,6 +131,7 @@ class ProjectInput {
         this.hostElement.insertAdjacentElement('afterbegin', this.element)
     }
 }
+
 
 
 const project = new ProjectInput();
